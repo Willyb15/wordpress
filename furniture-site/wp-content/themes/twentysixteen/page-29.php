62cd7166264ac01a89f1bcd2bd15f20c633f95fa
Willyb15 <william.steen.bryant@gmail.com>
2016-05-25 09:18:58 -0400

    gallery

---
+++ furniture-site/wp-content/themes/twentysixteen/page-29.php
@@ -0,0 +1,10 @@
+<?php
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	<?php echo photo_gallery(1); ?>
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+</div><!-- .content-area -->
+<?php get_footer(); ?>
